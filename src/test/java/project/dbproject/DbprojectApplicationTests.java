54beb6f5c5b5cae0aecf6cfa615881ebf2e8c8f4
Byuntil <gugugu2014@jbnu.ac.kr>
2024-05-26 13:27:00 +0900

    테스트 제거

--- src/test/java/project/dbproject/DbprojectApplicationTests.java
+++ src/test/java/project/dbproject/DbprojectApplicationTests.java
@@ -1,13 +1,8 @@
 package project.dbproject;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DbprojectApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
