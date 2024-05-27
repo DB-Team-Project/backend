649584fe56f0463f1127c8d94ca76f470a4bee2b
Sungjae Gu <74540758+Byuntil@users.noreply.github.com>
2024-05-28 03:31:17 +0900

    Update home.java

--- src/main/java/project/dbproject/controller/home.java
+++ src/main/java/project/dbproject/controller/home.java
@@ -1,11 +1,1 @@
-package project.dbproject.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-
-public class homeController {
-    public String home() {
-        return "index";
-    }
-}
